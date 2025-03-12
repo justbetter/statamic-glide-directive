97a702b82ac683372895896bcfe62f18d3fb387e
Kevin Meijer <kevin@justbetter.nl>
2025-03-12 15:48:48 +0100

    Dispatch glide job after http response when queue is not redis

--- src/Responsive.php
+++ src/Responsive.php
@@ -145,7 +145,11 @@ class Responsive
             return URL::encode($url);
         }
 
-        GenerateGlideImageJob::dispatchAfterResponse($asset, $preset, $fit, $format);
+        if (config('queue.default') === 'redis') {
+            GenerateGlideImageJob::dispatch($asset, $preset, $fit, $format);
+        } else {
+            GenerateGlideImageJob::dispatchAfterResponse($asset, $preset, $fit, $format);
+        }
 
         return null;
     }
