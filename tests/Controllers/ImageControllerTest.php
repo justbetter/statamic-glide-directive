db23dc07c200fb81d3826dfa01980a3abab9d71b
Kevin Meijer <kevin@justbetter.nl>
2025-05-01 11:47:39 +0200

    Updated tests

--- tests/Controllers/ImageControllerTest.php
+++ tests/Controllers/ImageControllerTest.php
@@ -2,12 +2,21 @@
 
 namespace JustBetter\GlideDirective\Controllers\Tests;
 
+use JustBetter\GlideDirective\Controllers\ImageController;
 use JustBetter\GlideDirective\Responsive;
 use JustBetter\GlideDirective\Tests\TestCase;
 use PHPUnit\Framework\Attributes\Test;
 
 class ImageControllerTest extends TestCase
 {
+    protected ImageController $controller;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->controller = app(ImageController::class);
+    }
+
     #[Test]
     public function it_returns_placeholder(): void
     {
@@ -24,5 +33,20 @@ class ImageControllerTest extends TestCase
         $presets = Responsive::getPresets($asset);
 
         $this->assertArrayHasKey('webp', $presets);
+    }
+
+    #[Test]
+    public function it_returns_404_for_missing_asset(): void
+    {
+        $this->expectException(\Symfony\Component\HttpKernel\Exception\NotFoundHttpException::class);
+
+        $this->controller->getPreset(
+            request(),
+            'xs',
+            'contain',
+            'dummy-signature',
+            'non-existent-file.jpg',
+            'jpg'
+        );
     }
 }
