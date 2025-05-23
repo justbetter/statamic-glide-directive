92cec5b95093724327e22771a896a55e889edf86
Jade Geels <a@jgw.tf>
2025-05-23 13:13:29 +0200

    Add extra fallback for nullish entries

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,7 +1,7 @@
 <script>
 window.responsiveResizeObserver = new ResizeObserver(async (entries) => {
     entries.forEach(entry => {
-        let imgWidth = entry?.devicePixelContentBoxSize?.[0]?.inlineSize || (entry.borderBoxSize[0]?.inlineSize * window.devicePixelRatio);
+        let imgWidth = entry?.devicePixelContentBoxSize?.[0]?.inlineSize || (entry?.borderBoxSize[0]?.inlineSize * window.devicePixelRatio) || 0;
         
         if (imgWidth === 0) {
             return;
